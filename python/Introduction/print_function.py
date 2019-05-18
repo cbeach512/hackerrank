ce42b4e9c8b2394bff7aa667852e526d993edb21
Chad Beach <cbeach512@gmail.com>
2019-05-18 13:32:04 -0400

    print function solution

---
+++ python/Introduction/print_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Print Function
+Read an integer N.
+Without using any string methods, try to print the following:
+    123...N
+  - Note that "..." represents the values in between.
+- Input Format
+The first line contains an integer N.
+- Output Format
+Output the answer as explained in the task.
+- Sample Input 0
+    3
+- Sample Output 0
+    123
+"""
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i + 1, end='')
+
